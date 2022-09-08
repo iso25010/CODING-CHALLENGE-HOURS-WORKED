06c17c53e6a854554e059509c8bde84bb53a9e8d
-Z- <iso250010@seznam.cz>
2022-09-08 21:38:55 +0200

    reverse combo to combo1

--- combo.py
+++ combo.py
@@ -3,7 +3,10 @@ nums = [1, 2, 3]
 
 comb=[{n-1:chars[:n-1]} for n in nums]
 comb1={n-1:chars[:n-1] for n in nums}
+comb2=[[comb1[x], x] for x in comb1]
 print(comb)
 #[{0: []}, {1: ['a']}, {2: ['a', 'b']}]
 print(comb1)
 #{0: [], 1: ['a'], 2: ['a', 'b']}
+print(comb2)
+#[[[], 0], [['a'], 1], [['a', 'b'], 2]]
