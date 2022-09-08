04a0d2c919081d2d992fe0ac20c21618ec74cc94
-Z- <iso25010@seznam.cz>
2022-09-08 15:31:22 +0200

    A new combo {0: [], 1: ['a'], 2: ['a', 'b']}

--- combo.py
+++ combo.py
@@ -2,4 +2,8 @@ chars = ['a', 'b']
 nums = [1, 2, 3]
 
 comb=[{n-1:chars[:n-1]} for n in nums]
+comb1={n-1:chars[:n-1] for n in nums}
 print(comb)
+#[{0: []}, {1: ['a']}, {2: ['a', 'b']}]
+print(comb1)
+#{0: [], 1: ['a'], 2: ['a', 'b']}
