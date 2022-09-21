807cf84d0bd69a085d19d30ac672fd02fcff3e1f
-Z- <iso250010@seznam.cz>
2022-09-21 20:56:31 +0200

    Imagine you are a young consultant, trying to help a small, very unorganized service company get their systems, and processes in order. On your first day the client asks you to help them with their basic employee data. This is the first task. How would you solve it?
    bonus : to further impress your client, convert into dictionary with a list of numbers as below
    employees_d2: {'Joe':[1,110],'Sue':[2,120],'Bo':[3,95],'Li':[4,90],'Ty':[5,80],'Vi':[6,86]}

---
+++ CONSULTANCY - DAY 1 .py
@@ -0,0 +1,6 @@
+#bonus : to further impress your client, convert into dictionary with a list of numbers as below
+#employees_d2: {'Joe':[1,110],'Sue':[2,120],'Bo':[3,95],'Li':[4,90],'Ty':[5,80],'Vi':[6,86]}
+
+employees_d2= {x[0]:[int(y) for y in x[1:]]  for x in [e.split(',')[:-1] for e in 'Joe,1,110,M*Sue,2,120,F*Bo,3,95,M*Li,4,90,F*Ty,5,80,M*Vi,6,86,F'.split('*')]}
+
+print(employees_d2)
