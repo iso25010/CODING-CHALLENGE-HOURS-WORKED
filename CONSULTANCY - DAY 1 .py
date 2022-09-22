68fb568b0684b8344e821840be8a487b4f173dce
-Z- <iso25010@seznam.cz>
2022-09-22 14:24:12 +0200

    Formated output

--- CONSULTANCY - DAY 1 .py
+++ CONSULTANCY - DAY 1 .py
@@ -3,4 +3,6 @@
 
 employees_d2= {x[0]:[int(y) for y in x[1:]]  for x in [e.split(',')[:-1] for e in 'Joe,1,110,M*Sue,2,120,F*Bo,3,95,M*Li,4,90,F*Ty,5,80,M*Vi,6,86,F'.split('*')]}
 
-print(employees_d2)
+for k,v in employees_d2.items():
+    print('Name: {0:<7} measures: {1:>3}, {2:>3}'.format(k,v[0],v[1]))
+
