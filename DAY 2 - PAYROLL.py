9339eeac1c538e731ef1de7b49c8f3f7b70a4c2d
-Z- <iso25010@seznam.cz>
2022-09-26 08:57:37 +0200

    Evidence creation

--- DAY 2 - PAYROLL.py
+++ DAY 2 - PAYROLL.py
@@ -11,8 +11,9 @@
 # bonus:
 # client would love if bonus of 1500 copper can be added for 1100+ calls
 
-#employee_d1:{Name:[employee number, base pay/hour]}
-employees_d1 = {'Joe': [1, 110], 'Sue': [2, 120], 'Bo': [3, 95],'Li': [4, 90],'Ty': [5, 80], 'Vi': [6, 86]}
+
+
+from datetime import date
 
 class Employee():
     def __init__(self,name,eNumber,base):
@@ -32,12 +33,28 @@ class Employee():
         rec="{0:}'s monthly pay is {1:} copper".format(self.name,self.getWage())
         return(rec)
 
+class Evidence():
+    def __init__(self):
+        self.lPhone_m=dict()
+        
+    def addPhone_m(self,date,phone_m):
+        self.lPhone_m[date]=phone_m
+
+    def __str__(self):
+        return self.lPhone_m.__str__()
+    
 
 
 # {'employee number':'logged hours at work', 'number of calls', 'total call-time in minutes','# feedback rated 4+'}
 phone_m = {1:[180,1200,4783,223],2:[175,1213,4565,275],3:[155,1008,4145,180],4:[160,1005,4315,138],5:[185,1265,6200,264],6:[160,1052,4532,184]}
 
+#employee_d1:{Name:[employee number, base pay/hour]}
+employees_d1 = {'Joe': [1, 110], 'Sue': [2, 120], 'Bo': [3, 95],'Li': [4, 90],'Ty': [5, 80], 'Vi': [6, 86]}
+
 e=Employee('Gogo',1,150)
 e.calculateWage(180)
 print(e)
 
+ev=Evidence()
+ev.addPhone_m(date(2022,9,6),phone_m)
+print(ev)
